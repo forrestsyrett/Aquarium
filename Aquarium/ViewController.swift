7770aa29b5de5e4c3e14307eee1d6074c5b6b8ef
forrestsyrett <forrest.syrett@me.com>
2016-06-11 11:45:23 -0600

    StoryBoard/UIAppearance

--- Aquarium/ViewController.swift
+++ Aquarium/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var gradientView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        
+        let gradientLayer = CAGradientLayer()
+        
+        view.backgroundColor = .blueColor()
+        gradientLayer.frame = view.bounds
+        let color1 = UIColor.yellowColor().CGColor as CGColorRef
+        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).CGColor as CGColorRef
+        gradientLayer.colors = [color1, color2]
+        
+        gradientLayer.locations = [0.0, 0.25, 0.50, 1.0]
+        
+        gradientView.layer.addSublayer(gradientLayer)
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
