84a4b773ae85763f5312a83b8bb18d0fb1019371
forrestsyrett <forrest.syrett@me.com>
2016-06-10 21:01:04 -0600

    Initial Commit

---
+++ Aquarium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aquarium
+//
+//  Created by Forrest Syrett on 6/10/16.
+//  Copyright © 2016 Forrest Syrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
