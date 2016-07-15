088935de8a255fad2da29c28e4f3ea5184d3d936
forrestsyrett <forrest.syrett@me.com>
2016-07-14 18:44:09 -0600

    Moved animation code into own file

---
+++ Aquarium/Animations.swift
@@ -0,0 +1,32 @@
+//
+//  Animations.swift
+//  Aquarium
+//
+//  Created by Forrest Syrett on 7/14/16.
+//  Copyright © 2016 Forrest Syrett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+func animateLogo(image: UIImageView) {
+    image.alpha = 0.0
+    UIView.animateWithDuration(2.0) {
+        image.alpha = 1.0
+    }
+}
+
+func animateLabel(label: UILabel, animateTime: Double) {
+    label.alpha = 0.0
+    UILabel.animateWithDuration(animateTime) {
+        label.alpha = 1.0
+    }
+}
+
+func animateLines(view: UIView, animateTime: Double) {
+    view.alpha = 0.0
+    UIView.animateWithDuration(animateTime) {
+        view.alpha = 1.0
+    }
+}
