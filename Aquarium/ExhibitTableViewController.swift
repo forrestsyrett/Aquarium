fd41a6e004d380ae5f9459ffc3048dfd94750e94
forrestsyrett <forrest.syrett@me.com>
2016-08-25 16:42:45 -0600

    StoryBoard fixes

--- Aquarium/ExhibitTableViewController.swift
+++ Aquarium/ExhibitTableViewController.swift
@@ -15,9 +15,6 @@ class ExhibitTableViewController: UIViewController, UITableViewDataSource  {
     @IBOutlet weak var exhibitGalleryLabel: UILabel!
     
     
-    
-    static let sharedController = ExhibitTableViewController()
-    
     var exhibitAnimals = [Animals]()
     var exhibitGalleryLabelMutable = "Exhibit Gallery"
     
@@ -31,11 +28,7 @@ class ExhibitTableViewController: UIViewController, UITableViewDataSource  {
         gradient(self.view)
         
     }
-    
-    func changeUtahLabel() {
-    exhibitGalleryLabel.text = "Discover Utah"
-    }
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         animalTableView.reloadData()
@@ -47,18 +40,18 @@ class ExhibitTableViewController: UIViewController, UITableViewDataSource  {
     }
     // MARK: - Table view data source
     
-     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
-     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return exhibitAnimals.count
     }
     
     
-     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("animalCell", forIndexPath: indexPath) as! AnimalTableViewCell
         
         // Configure the cell...
@@ -66,6 +59,7 @@ class ExhibitTableViewController: UIViewController, UITableViewDataSource  {
         let animal = exhibitAnimals[indexPath.row]
         cell.animalNameLabel.text = animal.info.name
         cell.animalPreviewImage.image = animal.info.animalImage
+        cell.backgroundColor = cell.contentView.backgroundColor
         roundCornerButtons(cell.animalPreviewImage)
         
         return cell
@@ -78,64 +72,40 @@ extension ExhibitTableViewController: UITableViewDelegate {
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-//        let moreInfoText = "Tap to learn more! >"
-        // 1
         guard let cell = tableView.cellForRowAtIndexPath(indexPath) as? AnimalTableViewCell else { return }
         
         let animal = exhibitAnimals[indexPath.row]
         
-        // 2
-        
         exhibitAnimals[indexPath.row] = animal
         
-        // 3
-       
-            cell.moreInfoTextField?.text = animal.info.description
-
-//            cell.moreInfoTextField.text = moreInfoText
-    
+        cell.moreInfoTextField?.text = animal.info.description
         
-        // 4
-        UIView.animateWithDuration(0.3) {
+        UIView.animateWithDuration(0.4) {
             cell.contentView.layoutIfNeeded()
         }
         
-        // 5
         animalTableView.beginUpdates()
         animalTableView.endUpdates()
         
-        // 6
         animalTableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Top, animated: true)
     }
-
+    
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
         let moreInfoText = "Tap to learn more! >"
-        // 1
         guard let cell = tableView.cellForRowAtIndexPath(indexPath) as? AnimalTableViewCell else { return }
         
         let animal = exhibitAnimals[indexPath.row]
         
-        // 2
-//        animal.info.isExpanded = false
         exhibitAnimals[indexPath.row] = animal
         
-        // 3
-                  cell.moreInfoTextField.text = moreInfoText
+        cell.moreInfoTextField.text = moreInfoText
         
-        
-        // 4
-        UIView.animateWithDuration(0.3) {
+        UIView.animateWithDuration(0.4) {
             cell.contentView.layoutIfNeeded()
         }
         
-        // 5
         animalTableView.beginUpdates()
         animalTableView.endUpdates()
-        
-        // 6
-        animalTableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Top, animated: true)
-        
-
     }
-
+    
 }
